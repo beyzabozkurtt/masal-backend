b38c36c04c58a7c98a098df4c58974612b9fb047
beyzabozkurtt <beyza.bozkurtt@gmail.com>
2025-06-08 16:05:29 +0300

    s

--- src/cloudinary/cloudinary.config.ts
+++ src/cloudinary/cloudinary.config.ts
@@ -1,9 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: 'dilxc99ki',
-  api_key: '543135174917351',
-  api_secret: 'WIpVJ6DzD43VsvuP-_kqE2IYyhM', // 👈 bunu “View API Keys” ile alacaksın
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 
 export default cloudinary;
